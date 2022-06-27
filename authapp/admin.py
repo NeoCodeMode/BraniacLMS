fad014a1780dcbacee8a8bd73d086ae52df93b8f
Nixon <48482549+NeoCodeMode@users.noreply.github.com>
2022-06-27 10:14:11 +0300

    Lesson 7 (#6)
    * lesson_7 Ready

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,13 +1,11 @@
-<<<<<<< HEAD
 from django.contrib import admin
 
-from authapp import models
+from authapp import forms, models
 
 
 @admin.register(models.CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
+    form = forms.CustomUserChangeForm
+    form_add = forms.CustomUserCreationForm
     list_display = ["id", "username", "email", "is_active", "date_joined"]
     ordering = ["-date_joined"]
-=======
-# Register your models here.
->>>>>>> master
